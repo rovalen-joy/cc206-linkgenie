84c76ff3ddb9181c1663a0f55f3f093062c2a2b0
Jaren Joseph Tanque <99623988+Ryohei06@users.noreply.github.com>
2023-10-03 16:46:56 +0800

    Modified the Code and Added the Generated QR Code

--- lib/features/generate_qrcode.dart
+++ lib/features/generate_qrcode.dart
@@ -7,7 +7,7 @@ class Generate_QR_Code extends StatelessWidget {
     // Define what should happen when the button is pressed
   }
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -23,13 +23,25 @@ class Generate_QR_Code extends StatelessWidget {
                 style: ButtonStyle(
                   backgroundColor:
                       MaterialStateProperty.all<Color>(Colors.orangeAccent),
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                      EdgeInsets.all(16.0), // You can adjust this value as needed
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    EdgeInsets.all(16.0), // You can adjust this value as needed
                   ),
                 ),
                 child: const Text(
                   'GENERATED QR CODE',
                   style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/qrcode.png',
+                    ),
+                    Text(
+                      'Input Date: hhtps://vm.tiktok.com/ZMjaQgKl2/',
+                    ),
+                  ],
                 ),
               ),
             ],
