2ad6c085fa49e82864b26f8bca0e3c5fac201cc8
Rovalen Joy Calaguing <99468139+rovalen-joy@users.noreply.github.com>
2023-10-03 18:41:28 +0800

    Modified the code and Added Buttons for Manually Color Picker

--- lib/features/input_and_color_selection.dart
+++ lib/features/input_and_color_selection.dart
@@ -9,81 +9,141 @@ class input_and_color_selection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth =
+        MediaQuery.of(context).size.width; //screen width for responsiveness
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('LinkGenie QR Code Generator'),
           backgroundColor: Colors.deepOrangeAccent,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 20), // Spacing from the top for padding
-            Row(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+              children: <Widget>[
+                SizedBox(height: 20), // Spacing from the top for padding
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.orangeAccent),
+                      ),
+                      child: const Text('URL'),
+                    ),
+                    SizedBox(
+                        width: 10), // Spacing between the URL and Text buttons
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.orangeAccent),
+                      ),
+                      child: const Text('Text'),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                    height: 10), // Spacing between the textfield and buttons
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0), // Padding
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter any URL or text to generate...',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: know,
                   style: ButtonStyle(
                     backgroundColor:
                         MaterialStateProperty.all<Color>(Colors.orangeAccent),
+                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      EdgeInsets.all(16.10), // Padding
+                    ),
                   ),
                   child: const Text(
-                    'URL',
+                    'CHANGE COLOR + ',
+                    style: TextStyle(color: Colors.black),
                   ),
                 ),
-                SizedBox(width: 10), // Spacing between the URL and Text buttons
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.orangeAccent),
-                  ),
-                  child: const Text(
-                    'Text',
+                SizedBox(height: 20), //for Spacing
+                Container(
+                  width: 0.8 * screenWidth,
+                  child: Image.asset(
+                    'assets/colorswatch.png',
+                    fit: BoxFit.fitWidth,
                   ),
                 ),
+                SizedBox(height: 20), //for Spacing
+                Container(
+                  width: 0.8 * screenWidth,
+                  child: Image.asset(
+                    'assets/colorpicker.png',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                SizedBox(height: 20), //for Spacing
+                Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, //Center two 'Select Manually' buttons in the row
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          right: 0.15 *
+                              screenWidth), //for spacing to the right of the first 'Select Manually' button
+                      child: ElevatedButton(
+                        onPressed: know,
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.orange),
+                          padding:
+                              MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0), //Padding
+                          ),
+                        ),
+                        child: const Text(
+                          'Select Manually',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 0.15 *
+                              screenWidth), //for spacing to the left of the second 'Select Manually' button
+                      child: ElevatedButton(
+                        onPressed: know,
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.orange),
+                          padding:
+                              MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0), //Padding
+                          ),
+                        ),
+                        child: const Text(
+                          'Select Manually',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20), //for Spacing
               ],
             ),
-            SizedBox(height: 10), // Spacing between the textfield and buttons
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16.0), // Padding
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter any URL or text to generate...',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-          Center(
-          child: Column(
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: know,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.orangeAccent),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    EdgeInsets.all(16.10), // You can adjust this value as needed
-                  ),
-                ),
-                child: const Text(
-                  'CHANGE COLOR + ',
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              Container(
-                child: Image.asset(
-                  'assets/colorswatch.png',
-                ),
-              ),
-            ],
           ),
-        ),
-          ],
         ),
       ),
     );
   }
-} 
-   
+}
