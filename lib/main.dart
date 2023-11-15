c8a9a2c4f4f16504b4310ad26c955c8e030a5ce0
Bethany Hope Cabristante <99635993+bethanyhopec@users.noreply.github.com>
2023-11-15 16:42:02 +0800

    Added Frequently Asked Questions Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cc206_linkgenie/features/frequently_asked_questions.dart';
 import 'package:flutter/material.dart';
 import 'features/input_and_color_selection.dart';
 import 'components/home_drawer.dart';
@@ -12,29 +13,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    var materialApp = MaterialApp(
+      title: 'FAQPage',
       theme: ThemeData(
-        // This is the theme of your application.
         //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const FAQPage(),
     );
+    return materialApp;
   }
 }
 
@@ -44,44 +32,48 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-  length: 1,
-  child: Scaffold(
-    appBar: AppBar(
-      title: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+      length: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'LINKGENIE',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto'),
+                ),
+                SizedBox(width: 8),
+                Image.asset(
+                  'assets/logo.png',
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: Colors.deepOrangeAccent,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.app_registration_outlined),
+                text: 'Input and Color Selection',
+              ),
+            ],
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.grey,
+          ),
+        ),
+        drawer: const HomeDrawer(),
+        body: const TabBarView(
           children: [
-            Text(
-              'LINKGENIE',
-              style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold, fontFamily: 'Roboto'),
-            ),
-            SizedBox(width: 8),
-            Image.asset(
-              'assets/logo.png',
-              height: 30,
-            ),
+            input_and_color_selection(),
           ],
         ),
       ),
-      backgroundColor: Colors.deepOrangeAccent,
-      bottom: TabBar(
-        tabs: [
-          Tab(
-            icon: Icon(Icons.app_registration_outlined),
-            text: 'Input and Color Selection',
-          ),
-        ],
-        labelColor: Colors.white,
-        unselectedLabelColor: Colors.grey,
-      ),
-    ),
-    drawer: const HomeDrawer(),
-    body: const TabBarView(
-      children: [
-        input_and_color_selection(),
-      ],
-    ),
-  ),
-);
+    );
   }
 }
