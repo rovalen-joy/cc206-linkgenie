6ecb4d2a5d126c631cb81381a071901363fab031
Rovalen Joy Calaguing <99468139+rovalen-joy@users.noreply.github.com>
2023-11-15 15:54:44 +0800

    Added a drawer in the About Us

---
+++ lib/components/home_drawer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 243, 108, 17),
+            ),
+            child: Text(
+              'Link Genie',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person_pin_rounded),
+            title: Text('About Us'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
