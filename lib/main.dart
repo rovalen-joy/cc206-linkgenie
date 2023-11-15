68dcf5bd3f6c6806e1a86fe30b166c8755827fbc
cherrymirracalisnao <cherrymirra.calisnao@wvsu.edu.ph>
2023-11-15 14:10:53 +0800

    I added an "About Us" screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'features/input_and_color_selection.dart';
-import 'components/home_drawer.dart';
+import 'features/about-us.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -33,55 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const About_Us(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-  length: 1,
-  child: Scaffold(
-    appBar: AppBar(
-      title: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'LINKGENIE',
-              style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold, fontFamily: 'Roboto'),
-            ),
-            SizedBox(width: 8),
-            Image.asset(
-              'assets/logo.png',
-              height: 30,
-            ),
-          ],
-        ),
-      ),
-      backgroundColor: Colors.deepOrangeAccent,
-      bottom: TabBar(
-        tabs: [
-          Tab(
-            icon: Icon(Icons.app_registration_outlined),
-            text: 'Input and Color Selection',
-          ),
-        ],
-        labelColor: Colors.white,
-        unselectedLabelColor: Colors.grey,
-      ),
-    ),
-    drawer: const HomeDrawer(),
-    body: const TabBarView(
-      children: [
-        input_and_color_selection(),
-      ],
-    ),
-  ),
-);
   }
 }
