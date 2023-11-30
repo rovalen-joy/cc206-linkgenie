7817f247c43ed57115a755db7ed3a70822384172
cherrymirracalisnao <cherrymirra.calisnao@wvsu.edu.ph>
2023-11-30 20:31:04 +0800

    Modified the code.

--- lib/features/about-us.dart
+++ lib/features/about-us.dart
@@ -9,15 +9,10 @@ class About_Us extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
+        centerTitle: true,
+        title:Text(
                 'ABOUT US',
                 style: TextStyle(
                   color: const Color.fromARGB(255, 255, 254, 254),
@@ -25,13 +20,11 @@ class About_Us extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Roboto',
                 ),
-              ),
-            ],
           ),
+          backgroundColor: Color.fromARGB(255, 243, 108, 17),
         ),
-        backgroundColor: Color.fromARGB(255, 243, 108, 17),
-      ),
-      body: SingleChildScrollView(
+        
+        body: SingleChildScrollView(
         child: Center(
           child: Container(
             padding: EdgeInsets.all(16.0),
@@ -40,32 +33,24 @@ class About_Us extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: screenWidth < 900 ? 16.0 : 400.0),
-                        child: Text(
-                          'LINKGENIE',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+                Container(
+                  width: 130,
+                  height: 110,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      'assets/link.png',
+                      fit: BoxFit.cover,
                     ),
-                  ],
+                  ),
                 ),
                 SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      width: screenWidth < 800 ? screenWidth - 32 : 650,
-                      height: 200,
+                      width: 270,
+                      height: 300,
                       decoration: BoxDecoration(
                         border: Border.all(
                           color: const Color.fromARGB(255, 255, 255, 255),
@@ -74,15 +59,16 @@ class About_Us extends StatelessWidget {
                         borderRadius: BorderRadius.circular(5),
                       ),
                       child: Padding(
-                        padding: const EdgeInsets.all(15.0),
+                        padding: const EdgeInsets.all(5.0),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
                               'LinkGenie QR Code Generator boasts an array of user-friendly features, simplifying the process of generating QR codes and enhancing the overall user experience with its user-friendly interface. LinkGenie presents an impressive set of features designed to simplify QR code generation making it more accessible and efficient.',
                               style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w500,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: 'Arimo',
                                 color: const Color.fromARGB(255, 0, 0, 0),
                               ),
                               textAlign: TextAlign.justify,
@@ -91,39 +77,26 @@ class About_Us extends StatelessWidget {
                         ),
                       ),
                     ),
-                    SizedBox(width: 20),
-                    Container(
-                      width: 200,
-                      height: 180,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.asset(
-                          'assets/link.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
                   ],
                 ),
-                SizedBox(height: 60),
-                Text(
+                Text( // This is for the Version Content
                   'VERSION 2.0',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 17, fontFamily: 'Anton'),
                 ),
                 SizedBox(height: 10),
                 Container(
-                  width: 700,
-                  height: 250,
+                  width: 650,
+                  height: 265,
                   decoration: BoxDecoration(
                     border: Border.all(
                       color: Colors.black,
-                      width: 5,
+                      width: 3,
                     ),
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
                         color: Color.fromARGB(255, 243, 108, 17),
-                        offset: Offset(10.0, 10.0),
+                        offset: Offset(7.0, 7.0),
                       ),
                     ],
                   ),
@@ -135,47 +108,56 @@ class About_Us extends StatelessWidget {
                         Text(
                           "What's New?",
                           style: TextStyle(
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'Anton',
                               color: const Color.fromARGB(255, 240, 237, 237)),
                         ),
-                        SizedBox(height: 20),
+                        SizedBox(height: 30),
                         Text(
-                          '   ✦  You are free to input any URL or text that you want to generate a QR Code.',
+                          '   ✦  You can generate QR Codes for any data, including text, URLs, PDFs, images, or videos of your choice.',
                           style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white),
+                            fontSize: 10.5,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            fontFamily: 'Arimo',
+                          ),
                         ),
                         SizedBox(height: 10),
                         Text(
                           '   ✦  You can automatically or manually select the colors for the generated QR code.',
                           style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white),
+                            fontSize: 10.5,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            fontFamily: 'Arimo',
+                          ),
                         ),
                         SizedBox(height: 10),
                         Text(
                           '   ✦  You can customize the QR Code colors by clicking the menu icon in the top left.',
                           style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white),
+                            fontSize: 10.5,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            fontFamily: 'Arimo',
+                          ),
                         ),
                         SizedBox(height: 10),
                         Text(
                           '   ✦  For your questions, the FAQs are also available in the menu.',
                           style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white),
+                            fontSize: 10.5,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            fontFamily: 'Arimo',
+                          ),
                         ),
                       ],
                     ),
                   ),
                 ),
-                SizedBox(height: 100),
+                SizedBox(height: 150),
                 Container(
                   child: Center(
                     child: Text(
@@ -183,19 +165,27 @@ class About_Us extends StatelessWidget {
                       style: TextStyle(
                           fontStyle: FontStyle.italic,
                           fontWeight: FontWeight.bold,
-                          fontSize: 18),
+                          fontSize: 13),
                     ),
                   ),
                 ),
                 Text(
-                    'Do you have questions or need help? Reach out to us through: '),
+                    'Do you have questions or need help? Reach out to us through: ',
+                    style: TextStyle(fontWeight: FontWeight.w300, 
+                    fontSize: 11),
+                    ),
                 Text(
                   'Email: supporttheshieldren@gmail.com',
                   style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      fontWeight: FontWeight.w500),
+                    decoration: TextDecoration.underline,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 11,),
                 ),
-                Text('Support Center: Linkgenie_theshieldren Help desk'),
+                Text('Support Center: Linkgenie_theshieldren Help desk',
+                  style: TextStyle(fontWeight: FontWeight.w300, 
+                  fontSize: 11),
+                  ),
+
                 SizedBox(height: 80),
                 Container(
                   child: Center(
@@ -207,12 +197,13 @@ class About_Us extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  width: 30,
-                  height: 30,
+                  width: 40,
+                  height: 40,
+                  padding: EdgeInsets.all(5.0),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(10),
                     child: Image.asset(
-                      'assets/logo.png',
+                      'assets/shieldrencorp.png',
                       fit: BoxFit.cover,
                     ),
                   ),
