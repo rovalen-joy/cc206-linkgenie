bab8dc26156735ec33bbb422bbbf970254bc37e0
Bethany Hope Cabristante <99635993+bethanyhopec@users.noreply.github.com>
2023-11-30 13:10:48 +0800

    Added different input type buttons and alert messages

--- lib/features/input_and_color_selection.dart
+++ lib/features/input_and_color_selection.dart
@@ -1,184 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart'; //Import for implementing color picker widget
+import 'package:qr_flutter/qr_flutter.dart'; //QR Flutter package for QR code generation
+import 'package:cc206_linkgenie/features/generate_qrcode.dart';
+import 'package:cc206_linkgenie/components/home_drawer.dart';
+import 'dart:ui'; // Import for using describeEnum
+import 'package:file_picker/file_picker.dart'; // Import for picking files from the device storage
+import 'dart:io'; // Import for using Dart's File and IO related classes
+import 'dart:typed_data'; // Import for working with byte data
 
-class input_and_color_selection extends StatelessWidget {
+//Enum repsents the different types of inputs for QR code generation
+enum InputType { text, url, pdf, image, video }
+
+//Created a StatefulWidget for input and color selection in the QR code generator
+class input_and_color_selection extends StatefulWidget {
   const input_and_color_selection({Key? key}) : super(key: key);
+  @override
+  _input_and_color_selectionState createState() =>
+      _input_and_color_selectionState();
+}
 
-  void know() {
-    // Define what should happen when the button is pressed
-  }
+//State class for the input and color selection screen
+class _input_and_color_selectionState extends State<input_and_color_selection> {
+  TextEditingController _textController = TextEditingController();
+  String data = '';
+  InputType _selectedInputType = InputType.text;
+  Color backgroundColor = Colors.white;
+  Color foregroundColor = Colors.black;
+  File? _selectedFile;
 
   @override
-  Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
+  void initState() {
+    super.initState();
+    _textController.addListener(_handleTextInputChange);
+  }
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 20),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Enter any URL or Text to generate',
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 50),
-                  ],
-                ),
-              ),
-              TextButton(
-                onPressed: know,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 252, 247, 242)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                    ),
-                  ),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                  ),
-                ),
-                child: Text(
-                  'QR Code Preview',
-                  style: TextStyle(color: Colors.black, fontSize: 17),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 300.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 5),
-                    borderRadius: BorderRadius.circular(30),
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    boxShadow: [
-                      BoxShadow(blurRadius: 5.0, offset: Offset(5.0, 5.0)),
-                    ],
-                  ),
-                  child: Image.asset('assets/qr.png'),
-                ),
-              ),
-              SizedBox(height: 90),
-              ElevatedButton(
-                onPressed: know,
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.orangeAccent),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    EdgeInsets.all(16.10), // Padding
-                  ),
-                ),
-                child: const Text(
-                  'CHANGE COLOR + ',
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 0.6 * screenWidth,
-                child: Image.asset(
-                  'assets/colorswatch.png',
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 0.6 * screenWidth,
-                child: Image.asset(
-                  'assets/colorpicker.png',
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                      right: 0.15 * screenWidth,
-                    ),
-                    child: ElevatedButton(
-                      onPressed: know,
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromARGB(255, 241, 184, 98)),
-                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          EdgeInsets.symmetric(
-                            vertical: 10.0,
-                            horizontal: 20.0,
-                          ),
-                        ),
-                      ),
-                      child: const Text(
-                        'Select Manually',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 0.15 * screenWidth,
-                    ),
-                    child: ElevatedButton(
-                      onPressed: know,
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromARGB(255, 241, 184, 98)),
-                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          EdgeInsets.symmetric(
-                            vertical: 10.0,
-                            horizontal: 20.0,
-                          ),
-                        ),
-                      ),
-                      child: const Text(
-                        'Select Manually',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 100),
-              ElevatedButton(
-                onPressed: know,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 235, 111, 9)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    EdgeInsets.symmetric(vertical: 25.0, horizontal: 25.0),
-                  ),
-                ),
-                child: const Text(
-                  'Generate QR Code',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 253, 250, 250), fontSize: 25),
-                ),
-              ),
-            ],
-          ),
+  //Method to handle changes in text input
+  void _handleTextInputChange() {
+    if (_selectedInputType == InputType.text ||
+        _selectedInputType == InputType.url) {
+      if (_isValidInput(_textController.text)) {
+        setState(() {
+          data = _textController.text;
+        });
+      } else {
+        setState(
+          () {
+            data = '';
+          },
+        );
+      }
+    }
+  }
+
+  //Method to validate the input based on the selected input type
+  bool _isValidInput(String input) {
+    switch (_selectedInputType) {
+      case InputType.text:
+        // For text, check if input is a URL, return false if it is
+        final urlPattern = RegExp(r'^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$');
+        return !urlPattern.hasMatch(input) && input.isNotEmpty;
+      case InputType.url:
+        // Validate URL format
+        final urlPattern = RegExp(r'^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$');
+        return urlPattern.hasMatch(input);
+      case InputType.pdf:
+        // Validate if input ends with .pdf (basic validation)
+        return input.toLowerCase().endsWith('.pdf');
+      case InputType.image:
+        // Validate if input ends with common image file extensions
+        final imagePattern =
+            RegExp(r'\.(jpeg|jpg|png|gif|bmp)$', caseSensitive: false);
+        return imagePattern.hasMatch(input);
+      case InputType.video:
+        // Validate if input ends with common video file extensions
+        final videoPattern =
+            RegExp(r'\.(mp4|mov|wmv|flv|avi)$', caseSensitive: false);
+        return videoPattern.hasMatch(input);
+      default:
+        return true;
+    }
+  }
+
+  //Method to start the QR code generation process
+  void generateQRCode() {
+    if (_selectedInputType == InputType.text ||
+        _selectedInputType == InputType.url) {
+      if (_textController.text.isEmpty) {
+        _showAlertDialog('You did not input anything!');
+      } else if (!_isValidInput(_textController.text)) {
+        _showAlertDialog('Invalid input for the selected type!');
+      } else {
+        setState(() {
+          data = _textController.text;
+        });
+        navigateToQRCodePage();
+      }
+    } else {
+      // Handling file inputs (pdf, image, video)
+      if (_selectedFile == null) {
+        _showAlertDialog('You did not upload any file!');
+      } else {
+        navigateToQRCodePage();
+      }
+    }
+  }
+
+//Method to navigate to the QR code generation page
+  void navigateToQRCodePage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Generate_QR_Code(
+          data: data,
+          foregroundColor: foregroundColor,
+          backgroundColor: backgroundColor,
         ),
       ),
     );
   }
-}
+
+//Method to show alert dialog in case of errors or invalid input
+  void _showAlertDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Alert'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
